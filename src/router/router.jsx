7c7c6bf87493c03417df8c3d39406378491ae188
sergiocouter <sergiocouter@gmail.com>
2023-07-12 13:55:20 -0300

    Instalado o react-router-dom e criado a pasta para rotas

---
+++ src/router/router.jsx
@@ -0,0 +1,9 @@
+import { createBrowserRouter } from "react-router-dom";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h1>Tech Flex</h1>,
+    children: [],
+  },
+]);
